b936bda979f01898cdcade4abc304ca62df5b7c9
Ara Kanic <ji.arakani@gmail.com>
2022-06-02 11:06:11 -0400

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -2,3 +2,12 @@
 function isPalindrome(x) {
     return x.toLowerCase() === x.split("").reverse().join("").toLowerCase() ? true : false
 }
+
+isPalindrome("a") // true
+isPalindrome("aba") // true
+isPalindrome("Abba") // true
+isPalindrome("hello") // false
+isPalindrome("Bob") // true
+isPalindrome("Madam") // true
+isPalindrome("AbBa") // true
+isPalindrome("") // true
